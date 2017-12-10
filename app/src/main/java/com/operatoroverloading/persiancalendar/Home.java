0b97c242a6df7cc71dcc558ec8779cb27006662c
nima <ijinima@gmail.com>
2017-12-10 14:10:31 +0330

    Nima ui

---
+++ app/src/main/java/com/operatoroverloading/persiancalendar/Home.java
@@ -0,0 +1,17 @@
+package com.operatoroverloading.persiancalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by nima on 12/10/2017 AD.
+ */
+
+public class Home extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
